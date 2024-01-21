394210a8278dab2bd8cc8e812ce691cd7e51c2ad
webua <vershinin.web@gmail.com>
2024-01-21 17:07:29 +0200

    rotation 5 francs

--- resources/js/5_francs_1940.js
+++ resources/js/5_francs_1940.js
@@ -17,73 +17,135 @@ class PAGE_APP {
         };
 
         this.container = null;
+        this.renderer = null;
+        this.camera = null;
+        
+        this.model = null;
+        this.initialRotationCompleted = false;
 
         this.init();
     }
+    
+
 
     init() {
         
         this.container = document.getElementById(this.options.containerID);
 
+		// Add event listener for window resize
+        window.addEventListener('resize', () => this.handleResize());
+        
+        // Add event listeners for mouse click and touchstart
+        this.container.addEventListener('mousedown', (event) => this.handleInteraction(event));
+        this.container.addEventListener('touchstart', (event) => this.handleInteraction(event));
+        
         this.calculations.chartWidth = Math.floor( this.container.clientWidth - 1 );
         this.calculations.chartHeight = Math.floor( this.calculations.chartWidth / this.options.aspectRatio );
 
         this.initScene();
     }
+    
+	handleResize(){
+		// Update the calculations based on the new size
+        this.calculations.chartWidth = Math.floor(this.container.clientWidth - 1);
+        
+        if(this.isFullscreen()){
+			this.calculations.chartHeight = Math.floor(this.container.clientHeight);
+		} else{
+	        this.calculations.chartHeight = Math.floor(this.calculations.chartWidth / this.options.aspectRatio);
+		}
+			
+
+        // Update the camera's aspect ratio
+        this.camera.aspect = this.calculations.chartWidth / this.calculations.chartHeight;
+        this.camera.updateProjectionMatrix();
+
+        // Update the renderer's size
+        this.renderer.setSize(this.calculations.chartWidth, this.calculations.chartHeight);
+	}
+    
+	handleInteraction(event){
+		
+		if('canvas' != event.target.nodeName.toLowerCase()) {
+			return;
+		}
+		
+        this.initialRotationCompleted = true;
+
+        // Remove the event listeners to prevent further checks
+        this.container.removeEventListener('mousedown', this.handleInteraction);
+        this.container.removeEventListener('touchstart', this.handleInteraction);
+
+    }
 
     initScene() {
         const scene = new THREE.Scene();
 
-        const camera = new THREE.PerspectiveCamera(60, this.calculations.chartWidth / this.calculations.chartHeight, 0.1, 1000);
-        //const camera = new THREE.OrthographicCamera(this.calculations.chartWidth / -this.cameraPlaneDivider, this.calculations.chartWidth / this.cameraPlaneDivider, this.calculations.chartHeight / this.cameraPlaneDivider, this.calculations.chartHeight / -this.cameraPlaneDivider, -200, 500);
-        camera.position.x = 0;
-        camera.position.y = 0;
-        camera.position.z = 68;
-        camera.lookAt( this.options.cameraTarget );
+        this.camera = new THREE.PerspectiveCamera(60, this.calculations.chartWidth / this.calculations.chartHeight, 0.1, 1000);
+        //this.camera = new THREE.OrthographicCamera(this.calculations.chartWidth / -this.cameraPlaneDivider, this.calculations.chartWidth / this.cameraPlaneDivider, this.calculations.chartHeight / this.cameraPlaneDivider, this.calculations.chartHeight / -this.cameraPlaneDivider, -200, 500);
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = 68;
+        this.camera.lookAt( this.options.cameraTarget );
 
 
         const light4 = new THREE.DirectionalLight(0xffffff, 1);
         light4.position.set(-150, 0, 150);
-        camera.add(light4);
+        this.camera.add(light4);
         
         const light5 = new THREE.DirectionalLight(0xffffff, 1);
         light5.position.set(150, 0, 150);
-        camera.add(light5);
+        this.camera.add(light5);
         
         const light6 = new THREE.DirectionalLight(0xffffff, 1);
         light6.position.set(0, 150, 150);
-        camera.add(light6);
+        this.camera.add(light6);
         
         const light7 = new THREE.DirectionalLight(0xffffff, 1);
         light7.position.set(0, -150, 150);
-        camera.add(light7);
+        this.camera.add(light7);
         
-        scene.add(camera);
+        scene.add(this.camera);
 
         // create a render and set the size
-        const renderer = new THREE.WebGLRenderer({
+        this.renderer = new THREE.WebGLRenderer({
             antialias: true,
         });
-        renderer.setClearColor(new THREE.Color(0x0A0A0A));
-        renderer.setSize(this.calculations.chartWidth, this.calculations.chartHeight);
-        renderer.shadowMap.enabled = false;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+        this.renderer.setClearColor(new THREE.Color(0x0A0A0A));
+        this.renderer.setSize(this.calculations.chartWidth, this.calculations.chartHeight);
+        this.renderer.shadowMap.enabled = false;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
         // add the output of the renderer to the html element
-        this.container.appendChild(renderer.domElement);
+        this.container.appendChild(this.renderer.domElement);
 
-        // IMPORTANT: make sure that your container.append(renderer.domElement); is executed BEFORE initializing Controls
-        const controls = new TrackballControls( camera, renderer.domElement );
+        // IMPORTANT: make sure that your container.append(this.renderer.domElement); is executed BEFORE initializing Controls
+        const controls = new TrackballControls( this.camera, this.renderer.domElement );
         controls.rotateSpeed = 3.6;
         controls.zoomSpeed = 0.8;
         controls.panSpeed = 1.1;
         controls.staticMoving= true;
         controls.keys = [ 'KeyA', 'KeyS', 'KeyD' ];
 
+		const clock = new THREE.Clock();
+
         const render = () => {
             requestAnimationFrame(render);
+            
+            if( this.initialRotationCompleted == false ){
+				// Get the elapsed time since the last frame
+				const delta = clock.getDelta();
+
+				// Rotate the loaded model around the X-axis with a specified speed
+				const rotationSpeed = 0.15; // Adjust the rotation speed as needed
+				if ( this.model ) {
+					this.model.rotation.x -= rotationSpeed * delta;
+				}
+			}
+
+            
             controls.update(); 
-            renderer.render(scene, camera);
+            this.renderer.render(scene, this.camera);
         };
 
         const loader = new GLTFLoader();
@@ -91,9 +153,9 @@ class PAGE_APP {
         const loadingProgress = document.getElementById('loading-progress');
 
         loader.load('./models/5_francs_1940_france.glb', (gltf) => {
-            const model = gltf.scene;
+            this.model = gltf.scene;
 
-            scene.add(model);
+            scene.add(this.model);
             render();
 
             // По завершении загрузки, скрываем индикатор загрузки
@@ -106,6 +168,13 @@ class PAGE_APP {
             }
             loadingProgress.innerHTML = `Loading: ${Math.round(percentComplete)}%`;
         });
+    }
+    
+	isFullscreen() {
+        return !!document.fullscreenElement ||
+               !!document.mozFullScreenElement ||
+               !!document.webkitFullscreenElement ||
+               !!document.msFullscreenElement;
     }
 }
 
